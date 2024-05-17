72f7eff880000d4c7a8654b5520b4cad36cc8973
ANUPVERMA <averma@mgmresorts.com>
2024-05-17 22:04:21 +0530

    refactoring

--- src/main/java/com/mgm/pd/cp/resortpayment/config/RouterHealthIndicator.java
+++ src/main/java/com/mgm/pd/cp/resortpayment/config/RouterHealthIndicator.java
@@ -1,21 +1,19 @@
 package com.mgm.pd.cp.resortpayment.config;
 
 import com.mgm.pd.cp.resortpayment.service.router.RouterClient;
+import lombok.AllArgsConstructor;
 import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.actuate.health.Health;
 import org.springframework.boot.actuate.health.HealthIndicator;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 
 @Component("intelligentRouter")
+@AllArgsConstructor
 public class RouterHealthIndicator implements HealthIndicator {
-    
     private static final Logger logger = LogManager.getLogger(RouterHealthIndicator.class);
-
-    @Autowired
     private RouterClient client;
 
     @Override
